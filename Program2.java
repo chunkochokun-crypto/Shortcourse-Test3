c109616f6a5be3caba92818151658f48ab8803f2
chunkochokun-crypto <chunko.chokun@gmail.com>
2026-09-19 15:32:39 +0700

    test 3

---
+++ Program2.java
@@ -0,0 +1,12 @@
+public class Program2 {
+    public static void main(String[] args) {
+        int size = 5;
+
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
